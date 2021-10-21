f6f41663fffedc9a02e6afc67372086aab64124f
Aadit Tambe <aadit.tambe@gmail.com>
2021-10-21 15:25:50 -0400

    x

--- police-app/police-log.r
+++ police-app/police-log.r
@@ -7,6 +7,9 @@ library(rsconnect)
 etl_umd_police_arrest_data = read_rds("etl_umd_police_arrest_data.rds")
 #etl_umd_police_incident_data = read_rds("../data/processed/etl_umd_police_incident_data.rds")
 etl_gwu_police_incident_log = read_rds("etl_gwu_incident_log.rds")
+# howard
+etl_howard_police_arrest_data = read_rds("etl_howard_incident_log.rds")
+etl_gt_police_arrest_data = read_rds("etl_gt_incident_log.rds")
 
 
 # CDS:Possession Marijuana L/T 10 grams
@@ -82,16 +85,24 @@ ui <- fluidPage(
         ), # closes tabItem = Dashboard
         tabItem(tabName = "gt", 
                 fluidRow(
-                  column(4, 
-                         "HI"
-                  )
-                  )
-                ),
+                  column(3, 
+                         selectInput(inputId = "select_crime_gt",
+                                     label = "Choose description",
+                                     list("theft", "drug violation"))
+                  ),
+                  
+                  column(9, plotOutput("gt_graph"))
+                )
+        ), 
         tabItem(tabName = "hd", 
                 fluidRow(
-                  column(4, 
-                         "HI"
-                  )
+                  column(3, 
+                         selectInput(inputId = "select_crime_hu",
+                                     label = "Choose description",
+                                     list("Student Misconduct", "Stalking"))
+                  ),
+                  
+                  column(9, plotOutput("howard_graph"))
                 )
         ), 
         tabItem(tabName = "umd", 
@@ -126,6 +137,7 @@ ui <- fluidPage(
 
 server <- function(input, output){
   
+  # Output 1
   output$table2 <- DT::renderDataTable(DT::datatable({
     
     etl_umd_police_arrest_data <- etl_umd_police_arrest_data[etl_umd_police_arrest_data$race == input$race, ]
@@ -140,6 +152,7 @@ server <- function(input, output){
     temp
   }))
   
+  # Output 2
   output$table3 <- DT::renderDataTable(DT::datatable({
     
     etl_gwu_police_incident_log <- etl_gwu_police_incident_log[etl_gwu_police_incident_log$nature_classification == input$nature_classification, ]
@@ -154,19 +167,33 @@ server <- function(input, output){
     temp
   }))
   
-  ## UMD plots
+  # output$table4 <- DT::renderDataTable(DT::datatable({
+  #   
+  #   etl_howard_police_arrest_data <- etl_howard_police_arrest_data[etl_howard_police_arrest_data$natures_of_crime == input$natures_of_crime, ]
+  #   
+  #   #%>% 
+  #   # filter(etl_umd_police_arrest_data$race == input$race)
+  #   
+  #   temp <- etl_howard_police_arrest_data %>% 
+  #     group_by(!!input$variable3) %>% 
+  #     count()
+  #   
+  #   temp
+  # }))
+  # etl_howard_police_arrest_data <- etl_howard_police_arrest_data[etl_howard_police_arrest_data$natures_of_crime == input$natures_of_crime]
   
+  
+  ## UMD plots output
+  ### create ractive dataframe
   umd_type_crime_by_year <- reactive({
     print(input$select_crime)
     req(input$select_crime)
     umd_graph = etl_umd_police_arrest_data[etl_umd_police_arrest_data$description == input$select_crime,]  %>% 
       group_by(year, description) %>% 
       summarise(number_crimes = n())
-    
-    
   })
   
-  
+  # creative a plot base don the reactive dataframe
   output$umd_plot = renderPlot({
     g<- ggplot(umd_type_crime_by_year(), aes(x = year, y = number_crimes))+
       geom_bar(stat = "identity")+
@@ -174,6 +201,59 @@ server <- function(input, output){
       ggtitle(paste0("UMD over the years: ", input$select_crime))
     
     g
+  })
+  
+  # Howard Output
+  
+  #etl_howard_police_arrest_data <- etl_howard_police_arrest_data[etl_howard_police_arrest_data$natures_of_crime == input$natures_of_crime]
+  
+
+  
+  ### create a howard reactive df
+  howard_type_crime_by_year <- reactive({
+    print(input$select_crime_hu) 
+    req(input$select_crime_hu)
+    howard_graph = etl_howard_police_arrest_data[etl_howard_police_arrest_data$natures_of_crimes == input$select_crime_hu,]  %>% 
+      group_by(year_reported, natures_of_crimes) %>% 
+      summarise(number_crimes = n())
+  })
+  
+  
+  
+    # cnow plot it~~~
+  output$howard_graph = renderPlot({
+    hu_graph<- ggplot(howard_type_crime_by_year(), aes(x = year_reported, y = natures_of_crimes))+
+      geom_bar(stat = "identity")+
+      theme(legend.position = "none")+
+      ggtitle(paste0("Howard ocver years: ", input$select_crime_hu))
+    
+    hu_graph
+    
+    
+  })
+  
+  
+  ### create a gt reactive df
+  gt_type_crime_by_year <- reactive({
+    print(input$select_crime_gt) 
+    req(input$select_crime_gt)
+    howard_graph = etl_gt_police_arrest_data[etl_gt_police_arrest_data$incident == input$select_crime_gt,]  %>% 
+      group_by(year_occured, incident) %>% 
+      summarise(number_crimes = n())
+  })
+  
+  
+  
+  # gt plot it~~~
+  output$gt_graph = renderPlot({
+    gt_graph<- ggplot(gt_type_crime_by_year(), aes(x = year_occured, y = incident))+
+      geom_bar(stat = "identity")+
+      theme(legend.position = "none")+
+      ggtitle(paste0("Georgetown ocver years: ", input$select_crime_gt))
+    
+    gt_graph
+    
+    
   })
   
 }
