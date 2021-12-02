87a1ee319235eb55bccc29f670eae0c92753387d
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2021-12-02 03:13:05 -0500

    nicks changes

--- police-app/police_log_final.r
+++ police-app/police_log_final.r
@@ -4,6 +4,7 @@ library(tidyverse)
 library(DT)
 library(rsconnect)
 library(janitor)
+library(plotly)
 
 umd_arrest = readRDS("C:/Users/nicho/Documents/GitHub/university_police_logs/police-app/umd_arrest.rds")
 umd_incident = readRDS("C:/Users/nicho/Documents/GitHub/university_police_logs/police-app/umd_incident.rds")
@@ -32,8 +33,34 @@ ui <- fluidPage(
                                      umd_incident_list)
                   ),
                   column(9, plotOutput("umd_incident_graph"))
+                ),
+                
+                fluidRow(
+                  column(12, plotlyOutput("umd_incident_time_graph"))
+                  
+                  
                 )
               ), # closes tabItem = Dashboard
+        
+        tabItem(tabName = "umd2",
+                
+                fluidRow(
+                  column(4,selectInput(inputId = "select_x",
+                                       label = "Choose incident",
+                                       list("year", "race"))),
+                  column(4,selectInput(inputId = "select_y",
+                                       label = "Choose incident",
+                                       umd_incident_list)),
+                  column(4,selectInput(inputId = "select_grouping",
+                                       label = "Choose incident",
+                                       umd_incident_list))
+                  
+                  
+                )
+                
+                
+                
+                ),
         tabItem(tabName = "download",
                 fluidRow(
                   selectInput("dataset", "Choose a dataset:", choices = c("University of Maryland", "Other")),
@@ -83,12 +110,32 @@ server <- function(input, output){
       geom_bar(stat = "identity")+
       theme(legend.position = "none")+
       ggtitle(paste0("UMD ", input$select_incident, " Incidents"))
-    
-    
-    
-    
   
 })
+  
+  # UMD Time Incident ---------------------------------
+  
+  
+  umd_incident_time = umd_incident %>% 
+    group_by(time_hour) %>% 
+    count()
+  
+  output$umd_incident_time_graph = renderPlotly({
+    p <- ggplot(data=umd_incident_time, aes(x=time_hour, y=n, group= 1, text = paste("Time of day: ", time_hour,"00 hours","<br>Total incidents reported: ", n))) +
+      geom_bar(stat="identity") +
+      ggtitle("Incidents reported by time of day")+
+      xlab("Time of day")+
+      ylab("Total number of incidents reported")
+    
+    
+    fig <- ggplotly(p, tooltip = "text")
+    
+    fig
+    
+    
+    
+  })
+  
   
 }
 
