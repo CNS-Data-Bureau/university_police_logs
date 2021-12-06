4707287538ae86ffd672f61b5c07a473d1c674cb
ndmvisuals <70617650+ndmvisuals@users.noreply.github.com>
2021-12-05 19:57:47 -0500

    arrest almost all formatted

--- police-app-arrests/police_log_final_arrest.R
+++ police-app-arrests/police_log_final_arrest.R
@@ -6,12 +6,22 @@ library(rsconnect)
 library(janitor)
 library(plotly)
 library(DT)
+library(gcookbook)
+library(hrbrthemes)
+library(RColorBrewer)
 
 umd_arrest = readRDS("./umd_arrest.rds")
 umd_arrest_combined = readRDS("./arrest_combined.rds")
 #umd_incident = readRDS("C:/Users/nicho/Documents/GitHub/university_police_logs/police-app/umd_incident.rds")
 umd_arrest_list = unique(umd_arrest$type)
-umd_arrest_list = c("All Incidents", umd_arrest_list)
+all_incident = "All Incident Types"
+umd_arrest_list = c(all_incident, umd_arrest_list)
+ls_years = unique(umd_arrest$year)
+min_year = min(ls_years)
+max_year = max(ls_years)
+
+color_scheme = "Dark2"
+single_color = "#1b9e77"
 
 
 ui <- fluidPage(
@@ -98,7 +108,7 @@ server <- function(input, output){
     #print(input$select_crime_hu) 
     req(input$select_incident)
     
-    if(input$select_incident == "All Incidents"){
+    if(input$select_incident == all_incident){
       umd_arrest_combined
       
     }
@@ -117,17 +127,28 @@ server <- function(input, output){
   # UMD arrest Graph
   output$umd_arrest_year_graph = renderPlot({
     
-    if(input$select_incident == "All Incidents"){
+    if(input$select_incident == all_incident){
       ggplot(df_umd_arrest_year(), aes(x = year, y = number, fill = final_type))+
         geom_bar(stat = "identity")+
-        ggtitle(paste0("UMD ", input$select_incident, " Arrests"))
+        labs(x = "Years", y = "Number of UMPD Cases",
+             title = paste0("UMD Arrests/Citations:  ", input$select_incident),
+             subtitle = "Broken Down By Top 3 Incidents Per Year",
+              fill ="Top 3 Incident Types")+
+        scale_fill_brewer(palette = color_scheme) +
+        theme_ipsum(grid="Y")+
+        scale_x_continuous( breaks = ls_years)
       
     }
     
     else{
-      ggplot(df_umd_arrest_year(), aes(x = year, y = number))+
+      ggplot(df_umd_arrest_year(), aes(x = year, y = number, fill = single_color))+
         geom_bar(stat = "identity")+
-        ggtitle(paste0("UMD ", input$select_incident, " Arrests"))
+        labs(x = "Years", y = "Number of UMPD Cases",
+             title = paste0("UMD Arrests/Citations:  ", input$select_incident))+
+        scale_fill_brewer(palette = color_scheme) +
+        theme_ipsum(grid="Y")+
+        scale_x_continuous( breaks = ls_years)+
+        theme(legend.position = "none")
       
       
     }
@@ -142,7 +163,7 @@ server <- function(input, output){
   df_umd_arrest_time = reactive({
     
     req(input$select_incident)
-    if(input$select_incident == "All Incidents"){
+    if(input$select_incident == all_incident){
       
       result_umd_arrest_time = umd_arrest %>% 
         distinct(year, umpd_case_number, .keep_all = TRUE) %>% 
@@ -161,27 +182,21 @@ server <- function(input, output){
       
       
     }
-    
-    
-    
-    
-    
-    
-    
     })
   
-  output$umd_arrest_time_graph = renderPlotly({
-    p <- ggplot(data=df_umd_arrest_time(), aes(x=time_hour, y=n, group= 1, text = paste("Time of day: ", time_hour,"00 hours","<br>Total arrests reported: ", n))) +
+  output$umd_arrest_time_graph =renderPlotly({
+    
+    ggplot(df_umd_arrest_time(), aes(x=time_hour, y=`n`, fill = single_color)) +
       geom_bar(stat="identity") +
-      
-      ggtitle("Arrest Cases by Time of Day")+
-      xlab("Time of day")+
-      ylab("Total number of arrests reported")
+      labs(x = "Time", y = "Number of Arrest/Citations",
+           title = paste0("UMD Arrests/Citations:  ", input$select_incident),
+           subtitle = paste0("By Hour, Aggregated from ", toString(min_year), "-", toString(max_year)))+
+      scale_fill_brewer(palette = color_scheme) +
+      theme_ipsum(grid="Y")+
+      scale_x_discrete( labels = c("12 a.m.", "1 a.m.", "2 a.m.", "3 a.m.", "4 a.m.", "5 a.m.", "6 a.m.", "7 a.m.","8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.",
+                                   "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.", "5 p.m.", "6 p.m.", "7 p.m.","8 p.m.", "9 p.m.", "10 p.m.", "11 p.m."))+
+      theme(legend.position = "none")
     
-    
-    fig <- ggplotly(p, tooltip = "text")
-    
-    fig
     
     
     
@@ -203,7 +218,7 @@ server <- function(input, output){
     #print(input$select_crime_hu) 
     req(input$select_incident)
     
-    if(input$select_incident == "All Incidents"){
+    if(input$select_incident == "All Incident Types"){
       
       if(input$checkbox_race == FALSE){
         result_umd_arrest_race_year = umd_arrest  %>% 
@@ -264,13 +279,28 @@ server <- function(input, output){
     
     ggplot(df_umd_arrest_race_year(), aes(x = year, y = num_people, fill = race))+
       geom_bar(stat = "identity", position = "dodge")+
-      ggtitle(paste0("UMD ", input$select_incident, " People Arrested or Cited by Race"))
+      labs(x = "Years", y = "Number of People",
+           title = paste0("UMD Arrests/Citations:  ", input$select_incident),
+           subtitle = "Broken Down By Race",
+           fill ="Race")+
+      scale_fill_brewer(palette = color_scheme) +
+      theme_ipsum(grid="Y")+
+      scale_x_continuous( breaks = ls_years)
     }
     
     else{
       ggplot(df_umd_arrest_race_year(), aes(x = race, y = num_people, fill = race))+
         geom_bar(stat = "identity", position = "dodge")+
-        ggtitle(paste0("UMD ", input$select_incident, " People Arrested or Cited by Race"))  
+        labs(x = "Years", y = "Number of People",
+             title = paste0("UMD Arrests/Citations:  ", input$select_incident),
+             subtitle = "Broken Down By Race",
+             fill ="Race")+
+        scale_fill_brewer(palette = color_scheme) +
+        theme_ipsum(grid="Y")
+        
+      
+      
+      
     }
     
     
